4750c3db78c5749e01e80317b9b0674b105ecf4b
Jennifer Fong <error@error.media>
2015-12-22 20:27:35 -0800

    testing session

--- index.js
+++ index.js
@@ -373,6 +373,7 @@ server.start(function (err) {
 
   io.set('authorization', (handshake, next) => {
     if (handshake.headers.cookie) {
+      console.log('got here!! ', handshake.headers.cookies)
       stateDefn.parse(handshake.headers.cookie, (err, state) => {
         if (state && state.secret) {
           let session = state.secret.uid;
