9f48aa095bd728fb718097a60381e06fe6163549
Jen <jfong@mozilla.com>
2016-01-18 13:52:10 -0800

    add more items

--- test/test.rooms.js
+++ test/test.rooms.js
@@ -40,7 +40,7 @@ describe('rooms', () => {
 
   it('should generate a mining grid', (done) => {
     rooms.generateMining('test', (err, grid) => {
-      Object.keys(grid).length.should.equal(25);
+      Object.keys(grid).length.should.equal(30);
       done();
     });
   });
